ace7ff0b476471acb643b9f048778f1743d84bf2
Faith Nzula Matei <faithnzula1230@gmail.com>
2022-08-09 01:29:56 +0300

    Completed Assignments

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+var employee = {
+    name: "Abby",
+    streetAddress:"Mua Hills"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+    const allEmployee = updateEmployeeWithKeyAndValue(
+        newEmployee,
+        "suzan"
+        
+        
+    )
+
+}
+employee.name;
+
+function deleteFromEmployeeByKey(employee, key , value) {
+    let cutEmployees = { ...employee }
+    cutEmployees[key] = value;
+    return cutEmployees;
+    let firedEmployee = deleteFromEmployeeByKey(
+        
+
+    )
+};
+delete cutEmployees.name;
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+     employee[key] = value;
+     return employee;
+    
+ }
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    const destructivelyDelete = destructivelyDeleteFromEmployeeByKey(employee, "roy");
+    destructivelyDelete;
+   }
